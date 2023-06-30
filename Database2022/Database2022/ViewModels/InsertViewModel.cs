cba2fab2c8dbdc3f65ac21eb18ea73af9fc97fbe
Alumno <Alumno@LAB1509-12>
2023-06-30 15:35:07 -0500

    Agregar archivos de proyecto.

---
+++ Database2022/Database2022/ViewModels/InsertViewModel.cs
@@ -0,0 +1,60 @@
+﻿
+using Database2022.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Xml.Serialization;
+using Xamarin.Forms;
+
+namespace Database2022.ViewModels
+{
+    internal class InsertViewModel : BaseViewModel
+    {
+        private string firstName;
+        public String Firstame
+        {
+            get { return firstName; }
+            set { SetValue(ref this.firstName, value); }
+        }
+        private string lastName;
+
+        public String LastName
+        {
+            get { return lastName; }
+            set { SetValue(ref this.lastName, value);}
+        }
+        private int personId;
+        public int PersonId
+        {
+            get { return personId; }
+            set { SetValue(ref this.personId, value); }
+        }
+        public ICommand InsertCommand { get; set; }
+
+        public InsertViewModel() {
+
+            PersonService service = new PersonService();
+
+            InsertCommand = new Command(() =>
+            {
+                PersonService service = new PersonService();
+                if (PersonId != 0)
+                {
+                    Console.WriteLine(PersonId);
+                    Firstame = "";
+                    LastName = "";
+                    PersonId = 0;
+                }
+                else
+                {
+                    int newPersonId = service.Get().Count + 1;
+                    service.Create(new Person { FirstName = Firstame,LastName = LastName, PersonId = newPersonId });
+                    firstName = "";
+                    lastName = "";
+                }
+                
+            });
+        }
+    }
+}
---
+++ Database2022/Database2022/Views/InsertPerson.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Database2022.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Database2022.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InsertPerson : ContentPage
+    {
+        public InsertPerson()
+        {
+            InitializeComponent();
+            this.BindingContext = new InsertViewModel();
+        }
+    }
+}
